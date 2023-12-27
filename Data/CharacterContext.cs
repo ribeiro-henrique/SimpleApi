3317666c3ab491b63e8a3192c87366ab6367003a
hesrr <hesrr@DESKTOP-38D5I26>
2023-12-27 11:46:14 -0300

    Add project files.

---
+++ Data/CharacterContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleApi.Models;
+
+namespace SimpleApi.Data
+{
+    public class CharacterContext : DbContext
+    {
+        public CharacterContext(DbContextOptions<CharacterContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Character { get; set; } = null!;
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleApi.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Role { get; set; }
+    }
+}
